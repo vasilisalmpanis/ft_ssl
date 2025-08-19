795f31fe6b4fc2431bec01fd41ad5811333bf507
Vasileios Almpanis <vasilisalmpanis@gmail.com>
2025-08-19 12:25:45 +0200

    ft_ssl: usage and help
    adds messages and functions to print
    usage and help
    
    Signed-off-by: Vasileios Almpanis <vasilisalmpanis@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # ft_ssl
 An introduction to cryptographic hashing algorithms
+
+#### Resources
+[rosetta](https://rosettacode.org/wiki/MD5/Implementation)
--- src/main.c
+++ src/main.c
@@ -1,6 +1,47 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+/*
+ * MD% steps
+ * 1. Input Padding
+ * 2. Length Appending
+ * 3. Buffer Initialization
+ * 4. Message Processing
+ * 5. Output
+ */
+
+#define USAGE_MSG "command [flags]... [file/string]...\n"
+
+#define HELP_MSG  "\nStandard commands:\n" \
+	"md5 sha256\n" \
+	"\nOptions:\n" \
+	"  -p	echo STDIN to STDOUT and append the checksum to STDOUT\n" \
+	"  -s	quiet mode\n" \
+	"  -q	reverse the format of the output\n" \
+	"  -r	print the sum of the given string\n" \
+	"\nExit status:\n" \
+	" 0  if OK,\n" \
+	" 1  if minor problems\n" \
+	" 2  if serious trouble (e.g., cannot access command-line argument).\n"
+
+void help()
+{
+	printf("%s", HELP_MSG);
+}
+
+void usage(char *name)
+{
+	printf("Usage: %s %s", name, USAGE_MSG);
+	help();
+}
+
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
+	char *program_name = argv[0];
+
+	if (argc == 1) {
+		usage(program_name);
+		exit(0);
+	}
 	return 0;
 }
